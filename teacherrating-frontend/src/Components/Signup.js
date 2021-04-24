9d64754dd164051adba2924647b5d0bf33c67f85
Yiming Chu <yimingdeveloper@gmail.com>
2021-04-23 20:01:01 -0700

    [bugfix] fix axe bugs

--- teacherrating-frontend/src/Components/Signup.js
+++ teacherrating-frontend/src/Components/Signup.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from "react";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import { useHistory } from "react-router-dom";
-import Backdrop from "@material-ui/core/Backdrop";
-import CircularProgress from "@material-ui/core/CircularProgress";
+import React, { useEffect, useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { useHistory } from 'react-router-dom';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const handleSubmit = (event) => {
   event.preventDefault();
@@ -20,16 +20,16 @@ const handleSubmit = (event) => {
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: "100%", // Fix IE 11 issue.
+    width: '100%', // Fix IE 11 issue.
     marginTop: theme.spacing(3),
   },
   submit: {
@@ -37,7 +37,7 @@ const useStyles = makeStyles((theme) => ({
   },
   backdrop: {
     zIndex: theme.zIndex.drawer + 1,
-    color: "#fff",
+    color: '#fff',
   },
 }));
 
@@ -48,10 +48,10 @@ export default function SignUp() {
 
   useEffect(() => {
     const fetchTeacher = async () => {
-      const resRaw = await fetch("/currentuser");
+      const resRaw = await fetch('/currentuser');
       const res = await resRaw.json();
       if (res.code === 200) {
-        history.push("/home");
+        history.push('/home');
       }
     };
     fetchTeacher();
@@ -81,7 +81,6 @@ export default function SignUp() {
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
-                  autoComplete="fname"
                   name="firstName"
                   variant="outlined"
                   required
@@ -99,7 +98,6 @@ export default function SignUp() {
                   id="lastName"
                   label="Last Name"
                   name="lastName"
-                  autoComplete="lname"
                 />
               </Grid>
               <Grid item xs={12}>
@@ -135,24 +133,24 @@ export default function SignUp() {
               onClick={async () => {
                 setLoading(true);
                 let formData = new FormData();
-                let firstName = document.getElementById("firstName").value;
-                let lastName = document.getElementById("lastName").value;
-                let email = document.getElementById("email").value;
-                let password = document.getElementById("password").value;
-                let userName = firstName + " " + lastName;
-                formData.append("userName", userName);
-                formData.append("email", email);
-                formData.append("password", password);
-                const raw = await fetch("/signup", {
-                  method: "post",
+                let firstName = document.getElementById('firstName').value;
+                let lastName = document.getElementById('lastName').value;
+                let email = document.getElementById('email').value;
+                let password = document.getElementById('password').value;
+                let userName = firstName + ' ' + lastName;
+                formData.append('userName', userName);
+                formData.append('email', email);
+                formData.append('password', password);
+                const raw = await fetch('/signup', {
+                  method: 'post',
                   body: formData,
                 });
                 const res = await raw.json(); // parses JSON response into native JavaScript objects
                 setLoading(false);
                 if (res.code === 200) {
-                  history.push("/");
+                  history.push('/');
                 } else {
-                  alert("Sign up error.");
+                  alert('Sign up error.');
                 }
               }}
             >
